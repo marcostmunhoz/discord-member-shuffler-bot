faa670666e28efae3826a86895befee259714242
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-20 22:55:38 -0300

    refactor: Add start script and error handling

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const {Client, Intents} = require('discord.js')
+const { Client } = require('discord.js')
 
-const { GUILDS, GUILD_MEMBERS, GUILD_MESSAGES } = Intents.FLAGS;
 const client = new Client({
-    intents: [GUILDS, GUILD_MEMBERS, GUILD_MESSAGES]
+    intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
 });
 
+try {
+    client.on('ready', () => {
+        console.log(`Bot started successfully. Currently logged in as '${client.user.tag}'`);
+    })
 
-client.on('ready', () => {
-    console.log(`Bot started successfully. Currently logged in as '${client.user.tag}'`);
-})
+    client.on('messageCreate', async message => {
+        const {content, guild} = message;
 
-client.on('messageCreate', async message => {
-    const {content, guild} = message;
+        if (content === '!shuffle members') {
+            const {members} = guild;
 
-    if (content === '!shuffle members') {
-        const {members} = guild;
+            const list = await members.list()
+            const names = list
+                .map(member => member.displayName)
+                .sort(() => 0.5 - Math.random());
+            let response = 'Here\'s the shuffled list of server members:\n';
 
-        const list = await members.list()
-        const names = list
-            .map(member => member.displayName)
-            .sort(() => 0.5 - Math.random());
-        let response = 'Here\'s the shuffled list of server members:\n';
+            for (let index in names) {
+                response += `\n${parseInt(index) + 1}) ${names[index]}`;
+            }
 
-        for (let index in names) {
-            response += `\n${parseInt(index) + 1}) ${names[index]}`;
+            await message.reply(response);
         }
+    })
 
-        await message.reply(response);
-    }
-})
-
-client.login(process.env.DISCORD_BOT_TOKEN)
+    client.login(process.env.DISCORD_BOT_TOKEN)
+} catch (e) {
+    console.error('There was an error', e);
+}
