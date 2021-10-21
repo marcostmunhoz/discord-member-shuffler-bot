9cdb5d19f24353a91d464875431ef759eb318231
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-20 23:49:24 -0300

    refactor: Changes shuffling algorithm

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const client = new Client({
     intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
 });
 
+/**
+ * Shuffles array in place. ES6 version
+ * 
+ * @param {Array} array items An array containing the items.
+ * @returns {Array}
+ * 
+ * @see https://stackoverflow.com/a/6274381
+ */
+ function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
+
 try {
     client.on('ready', () => {
         console.log(`Bot started successfully. Currently logged in as '${client.user.tag}'`);
@@ -15,10 +33,8 @@ try {
         if (content === '!shuffle members') {
             const {members} = guild;
 
-            const list = await members.list()
-            const names = list
-                .map(member => member.displayName)
-                .sort(() => 0.5 - Math.random());
+            const list = await members.fetch()
+            const names = shuffle(list.map(member => member.displayName));
             let response = 'Here\'s the shuffled list of server members:\n';
 
             for (let index in names) {
